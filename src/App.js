a95c90778f1d2e8efa527d5fd7483ca4bf6e6560
Achyut <rastogiachyut@gmail.com>
2023-03-21 19:42:40 +0530

    Add styles logic, key use logic

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
-import { useRef, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
+const cities = ['Goa', 'Delhi', 'Bangalore', 'Ghatkopar', 'Gondolin']
 
 const MyDropdown = ({options, api = ''}) => {
   console.log('options', options)
   const [query, setQuery] = useState('')
   const [show, setShow] = useState(false)
+  const [focusedIndex, setFocusedIndex] = useState(-1)
   const selectInput = useRef();
+  const selectList = useRef(null);
+  
+  useEffect(() => {
+    if (!selectList.current) return
+    selectList.current.scrollIntoView({
+      block: "center"
+    }, [focusedIndex])
+  })
 
 
   const focusOut = (e) => {
@@ -28,20 +37,52 @@ const MyDropdown = ({options, api = ''}) => {
     setQuery(opt)
   }
 
+  const handleKeyDown = (e) => {
+    const { key } = e
+    console.log('handleKeyDown', key)
+    console.log('e.target.value', e.target.value)
+    let nextIndexCount = 0
+    
+    if (key === 'ArrowDown') {
+      nextIndexCount = (focusedIndex + 1) % cities.length
+    }
+    if (key === 'ArrowUp') {
+      nextIndexCount = (focusedIndex - 1) % cities.length
+    }
+    if (key === 'Enter') {
+      setSelectValue(cities[focusedIndex])
+    }
+    if (key === 'Escape') {
+      setShow(false)
+    }
+    setFocusedIndex(nextIndexCount)
+  }
+
   return (
-    <div onBlur={focusOut} ref={selectInput} className='container'>
-      <input value={query} onFocus={focusIn} onChange={e => setSelectValue(e.target.value)} />
+    <div onBlur={focusOut} ref={selectInput} className='dropdown'>
+      <div tabIndex={1} onKeyDown={handleKeyDown} className={`input-container ${show ? "open" : ""}`}>
+        <input value={query} onFocus={focusIn} onChange={e => setSelectValue(e.target.value)} />
+      </div>
+      {console.log('options', options)}
       {show ? <ul className='options'>
         {options.filter(opt => opt.toLowerCase().startsWith(query.toLowerCase()))
-                .map(opt => <li className='option' key={opt} onMouseDown={() => setSelectValue(opt)}>{opt}</li>)}
+                .map((opt, index) => {
+                  return (
+                    <li
+                      ref={index === focusedIndex ? selectList : null}
+                      style={{backgroundColor: index === focusedIndex ? "rgba(0,0,0,0.1)" : ""}}
+                      className='option'
+                      key={opt}
+                      onMouseDown={() => setSelectValue(opt)}
+                    >
+                      {opt}
+                    </li>)}
+                  )
+                }
       </ul> : null}
     </div>
   )
 }
-
-
-const cities = ['Goa', 'Delhi', 'Bangalore', 'Ghatkopar']
-
 
 function App() {
   return (
